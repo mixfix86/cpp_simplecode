6b54abf2e90406df628d29d05b6d6082d489a686
mix <mixfix@gmail.com>
2023-03-27 17:57:14 +0500

    8 урок

--- First_project/main.cpp
+++ First_project/main.cpp
@@ -2,9 +2,22 @@
 
 using namespace std;
 
-void main()
+#include<iostream>
+using namespace std;
+
+int main()
 {
-	// locale console2
 	setlocale(LC_ALL, "Rus");
-	cout << "Максим"<<endl;
+
+	int a, b, c;
+
+	cout << "Введите число 1:";
+	cin >> a;
+	cout << "Введите число 2:";
+	cin >> b;
+	cout << "Введите число 3:";
+	cin >> c;
+	cout << "Cумма чисел: " << a + b + c << endl;
+	cout << "Произведение чисел: " << a * b * c << endl;
+	cout << "Среднее арифметическое: " << double(a + b + c) / 3 << endl;
 }
